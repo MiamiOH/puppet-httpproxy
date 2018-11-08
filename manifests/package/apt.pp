9a11b5cb291cb2e335fa778eabfb6b91fb74c5df
Ralph Bolton <ralph.bolton@pre-emptive.net>
2018-11-08 10:46:46 +0000

    Reformatted to resolve lint failures

--- manifests/package/apt.pp
+++ manifests/package/apt.pp
@@ -2,11 +2,11 @@
 # Writes a conf.d file to make Apt use the proxy
 class httpproxy::package::apt {
   file { 'apt_via_proxy':
-    path    => '/etc/apt/apt.conf.d/05proxy',
     ensure  => $httpproxy::packagemanager::ensure,
+    path    => '/etc/apt/apt.conf.d/05proxy',
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => "Acquire::http::Proxy \"http://${httpproxy::http_proxy}:${httpproxy::http_proxy_port}\";"
+    content => "Acquire::http::Proxy \"http://${httpproxy::http_proxy}:${httpproxy::http_proxy_port}\";",
   }
 }
